570135f160abc8e41943d031797edd82d431be08
desa <855040@gmail.com>
2023-02-13 14:43:12 +0100

    initial commit, feed item

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,8 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by desanka milakovic on 13.2.23..
+//
+
+import Foundation
---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,8 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by desanka milakovic on 13.2.23..
+//
+
+import Foundation

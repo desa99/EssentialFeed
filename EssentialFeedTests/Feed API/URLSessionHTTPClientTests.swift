72f921eb86fe208fdaf875a4c51f25c3e180466e
desa <855040@gmail.com>
2023-11-20 15:54:58 +0100

    Move the URLSessionHTTPClient(the system under test, or 'SUT') creation to a factory methodto protect our tests from breaking changes

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class HTTPClientURLSession {
+class URLSessionHTTPClient {
   
     let session: URLSession
     init(session: URLSession = .shared) {
@@ -35,29 +35,30 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_performGETRequestWithURL() {
-        let sut = HTTPClientURLSession()
+        
         let url = URL(string: "http://any-url")!
         
-        sut.get(from: url) {_ in}
-        
         let exp = expectation(description: "Wait for completion")
+        
         URLProtocolStub.observeRequest { request in
             XCTAssertEqual(request.url, url)
             XCTAssertEqual(request.httpMethod, "GET")
             exp.fulfill()
         }
+        
+        makeSUT().get(from: url) {_ in}
+        
         wait(for: [exp], timeout: 1.0)
     }
 
     func test_getFromURL_failsOnRequestError() {
-        let sut = HTTPClientURLSession()
         let url = URL(string: "http://any-url")!
         let error = NSError(domain: "any error", code: 1)
         
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         
         let exp = expectation(description: "Wait for completion")
-        sut.get(from: url) { (result) in
+        makeSUT().get(from: url) { (result) in
             switch result {
             case let .failure(receivedResult as NSError):
                 XCTAssertEqual(receivedResult.domain, error.domain)
@@ -71,6 +72,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    // Helper
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
         private static var requestObserver: ((URLRequest) -> Void)?
